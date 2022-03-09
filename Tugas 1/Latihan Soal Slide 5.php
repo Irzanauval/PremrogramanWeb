14bcd5fd698e017ff22eb0b94bd14447b6d45c83
Irzanauval <99639418+Irzanauval@users.noreply.github.com>
2022-03-09 21:50:16 +0700

    Add files via upload

---
+++ Tugas 1/Latihan Soal Slide 5.php
@@ -0,0 +1,12 @@
+<?php
+
+var_dump(1 + 1);
+var_dump("1 + 1");
+var_dump( 1 + "1");
+var_dump( "1" + "1");
+
+//var_dump(1 + 1); akan mengeluarkan output 2 yang bertipe data interjer  
+//var_dump("1 + 1"); akan mengeluarkan output "1 + 1" yang bertipe data string berisi 4 karakter
+//var_dump( 1 + "1"); akan mengeluarkan output 2 yang bertipe data intejer
+//var_dump( "1" + "1"); akan mengeluarkan output 2 yang bertipe data intejer
+?>
